73f0670bfa6ac0c5238113cd4e2fcb43c3541dfe
Sangram Das <sangramraju143@gmail.com>
2024-09-18 01:45:30 +0530

    next line

--- Basic.cpp
+++ Basic.cpp
@@ -1,4 +1,14 @@
 #include<iostream>
  int main(){
     std::cout << "Hii ,Sangram";
+    return 0;
  }
+
+
+ /*
+  *  for next line we can use std::cout <<"Hii Sangram"; <<"\n";
+  * for next line we can use std::cout <<"hii sangram" << std::endl;
+  *  we can use  // using namespace std;
+  
+  
+  */
