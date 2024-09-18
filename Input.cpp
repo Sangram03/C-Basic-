5d002414bbf57bb340dafb03677cb617f871df81
Sangram Das <sangramraju143@gmail.com>
2024-09-18 13:31:35 +0530

    new file datatype

--- Input.cpp
+++ Input.cpp
@@ -6,3 +6,11 @@
  cout <<"value of X:"<< x;
  return 0;
  }
+
+ /*
+ * This is a simple C++ program that takes an integer input from the user and
+ * prints it back to the console.
+ * for two input we can use // cin >> x >> y;
+ * for  two output we can use cout // cout <<"Value of x" <<x << "and y" << y;
+ * for all library we can use #include<bits/stdc++.h>
+ */
---
+++ datatype.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+    //int
+    int x=10;
+    //long
+    long x =15;
+    cin >> x;
+
+
+    long long x = 15000000000; 
+
+
+    // float ,double 
+
+    float x = 5.6;
+    float y =5;
+
+    return 0
+}
+
+
+/*
+* int are 4 bytes and the range between them is -2,14,74,83,648 to 2,14,74,83,647
+* long long are 8 bytes and the range between them is -9,223,372,
+* float are 4 byte and range 1.2E-38 to 3.4E+38
+* double are 8 byte and range 2.2E-308 to 1.8
+* long double are 10 byte and range is 3.4E-4932 to 1.1E+4932
+*/
